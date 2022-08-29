3a3e29bb8606e9090903d8e7e4b4ab52e56335e7
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-08-29 20:44:37 -0300

    implementing getters for Feedback class

--- src/Model/Feedback.php
+++ src/Model/Feedback.php
@@ -13,8 +13,18 @@ class Feedback
         if ($nota < 9 && empty($depoimento)) {
             throw new \DomainException('Depoimento obrigatório');
         }
-        
+
         $this->nota = $nota;
         $this->depoimento = $depoimento;
+    }
+
+    public function recuperarNota(): int
+    {
+        return $this->nota;
+    }
+
+    public function recuperarDepoimento(): ?string
+    {
+        return $this->depoimento;
     }
 }
